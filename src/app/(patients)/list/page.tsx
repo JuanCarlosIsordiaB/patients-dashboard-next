4bdd9cbaec58ad9524b76996e0b5ae08c7d39b2e
JuanCarlosIsordiaB <juancarlosisordiab@gmail.com>
2024-04-19 16:01:52 -0600

    first commit

---
+++ src/app/(patients)/list/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function CreatePage() {
+    return (
+      <div className="container mx-auto mt-20">
+        <h1>Follow Your <span>Patients</span> </h1>
+      </div>
+    );
+  }
