e57b4875dc3f2c07ede3f4bc51a833db91d64505
JuanCarlosIsordiaB <juancarlosisordiab@gmail.com>
2024-04-24 12:48:49 -0600

    finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
-
-import { Metadata } from "next";
-import Image from "next/image";
-import { redirect } from "next/navigation";
-
-
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
 
-  redirect('/create');
-  
+  useEffect(() => {
+    router.push("/create");
+  }, [router]);
+
+  return (
+    <div>
+      <h1>Redirecting...</h1>
+    </div>
+  );
 }
